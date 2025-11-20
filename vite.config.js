d76d8a158c30062d715c4facef816c526524b2bd
Iosif Chatzimichail <afonic@users.noreply.github.com>
2025-11-20 10:38:40 +0200

    Simplify vite config

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,20 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    plugins: [
-        tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
     build: {
         manifest: true,
         outDir: 'dist',
         rollupOptions: {
-            input: {
-                'easy-forms': 'resources/js/frontend.js',
-            },
+            input: { 'easy-forms': 'resources/js/frontend.js' },
             output: {
                 entryFileNames: 'js/[name].js',
                 chunkFileNames: 'js/[name]-[hash].js',
                 assetFileNames: (assetInfo) => {
-                    if (assetInfo.name && assetInfo.name.endsWith('.css')) {
-                        return 'css/[name][extname]'
-                    }
-                    return 'assets/[name]-[hash][extname]'
+                    // CSS files go to css/, everything else to assets/
+                    return assetInfo.names[0]?.endsWith('.css')
+                        ? 'css/[name][extname]'
+                        : 'assets/[name]-[hash][extname]'
                 },
             },
         },
